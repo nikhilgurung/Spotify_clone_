be099080a9b913e7e3972d1abe3ec82177402984
Nikhil Gurung <nikkhilgurung@gmail.com>
2024-07-09 20:25:39 +0530

    first commit

---
+++ Spotify Clone/spotify.js
@@ -0,0 +1,24 @@
+async function getsongs(){
+    let a = await fetch("http://127.0.0.1:5500/Spotify%20Clone/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+        
+    }
+    return songs
+}
+async function main(){
+    let songs = await getsongs()
+    console.log(songs)
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+}
+main()
